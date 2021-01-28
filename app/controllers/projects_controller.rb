c3f0f30d8f00b983c19882a9dc82e3ac9f05731e
Kenneth Johnson C <kennojc@gmail.com>
2021-01-28 20:04:22 -0300

    Defined create method

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,17 @@
 class ProjectsController < ApplicationController
+  
+  http_basic_authenticate_with name: "desafiovamoscontodo", password: "XAHTJEAS23123%23", only:
+  :dashboard
+
   def index
   end
 
   def dashboard
+    @projects = Project.all
   end
+
+  def create
+    @project = Project.create(name: params[:name], description: params[:description], start_date: params[:start_date], end_date: params[:end_date], status: params[:status] )
+  end
+
 end
